980f6afa71ea28fdbcfcbd3e2699c850fd5737f4
Michael Schwartz <mykesx@gmail.com>
2018-11-05 10:45:10 -0800

    fix component.mk to build Types (#89)

--- component.mk
+++ component.mk
@@ -3,7 +3,7 @@
 
 CFLAGS += -DLIBXMP_CORE_PLAYER -DLIBXMP_CORE_DISABLE_IT
 
-COMPONENT_PRIV_INCLUDEDIRS=. src src/Widgets src/libxmp src/libxmp/loaders
+COMPONENT_PRIV_INCLUDEDIRS=. src src/Types src/Widgets src/libxmp src/libxmp/loaders
 # COMPONENT_EXTRA_INCLUDES=../src ../../src ../../../src ../../../../src
 COMPONENT_SRCDIRS=${COMPONENT_PRIV_INCLUDEDIRS}
 
